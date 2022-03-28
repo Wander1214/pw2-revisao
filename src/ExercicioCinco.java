fd0ad15d4c8747f30d29a3b884b99882b30967c7
wanderlei-luiz <wanderlei-luiz@test.com>
2022-03-28 10:21:54 -0300

    Desenvolvimento do exercicio 5

---
+++ src/ExercicioCinco.java
@@ -0,0 +1,16 @@
+
+public class ExercicioCinco {
+
+	public static void main(String[] args) {
+	
+	double trabalhoDeHistoria = 8.0;
+	double trabalhoDeMatematica = 7.0;
+	double trabalhoDeCiencia = 10;
+	double trabalhoDeGeografia = 9.5;
+	
+	double media = (trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia)/4 ;
+    
+	System.out.println ("Media = "+ media);
+	
+}
+}
